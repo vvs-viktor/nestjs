35abe143258a547ab7a68c05d0fe7d07c1e64ee2
viktorsakhno <sakhnoddc@gmail.com>
2021-09-21 10:36:58 +0300

    initial commit_3

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     .setTitle('Test nest')
     .setDescription('The cats API description')
     .setVersion('1.0')
-    // .addTag('test')
+    .addTag('test')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
