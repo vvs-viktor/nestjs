5f45d75d66375a9f05b0c5db9fcca667186018dc
viktorsakhno <sakhnoddc@gmail.com>
2021-09-24 15:16:36 +0300

    Added new functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,15 @@ import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { BotModule } from './bot/bot.module';
+import {ScheduleModule} from '@nestjs/schedule';
 
 @Module({
   imports: [
-    ProductsModule,
     MongooseModule.forRoot(
       `mongodb+srv://viktor:njh77889911@cluster0.37vqz.mongodb.net/products?retryWrites=true&w=majority`,
     ),
+    ScheduleModule.forRoot(),
+    ProductsModule,
     AuthModule,
     UsersModule,
     BotModule,
--- src/bot/bot.gateway.ts
+++ src/bot/bot.gateway.ts
@@ -2,6 +2,8 @@ import { Injectable, Logger } from '@nestjs/common';
 import {Once, ClientProvider, Client, OnCommand, On} from 'discord-nestjs';
 import {Message} from 'discord.js';
 import {UsersService} from '../users/users.service';
+import {Cron, Interval, SchedulerRegistry, Timeout} from '@nestjs/schedule';
+import { parse } from "discord-command-parser";
 
 @Injectable()
 export class BotGateway {
@@ -9,35 +11,48 @@ export class BotGateway {
 
   constructor(
     private readonly userService: UsersService,
+    private schedulerRegistry: SchedulerRegistry
   ) {}
 
   @Client()
   discordProvider: ClientProvider;
 
-  @Once({ event: 'ready' })
-  onReady(): void {
-    this.logger.log(
-      `Logged in as ${this.discordProvider.getClient().user.tag}!`,
-    );
-  }
+  // @Once({ event: 'ready' })
+  // @Interval(60000)
+  // onReady(): void {
+  //   const channel = this.discordProvider.getClient().channels.cache.get('890194752778174468')
+  //   if(channel.isText()) {
+  //     channel.send('Hello. One minute passed!!!')
+  //   }
+  // }
 
-  @OnCommand({ name: 'test' })
+  @OnCommand({ name: 'S' })
   async onCommand(message: Message): Promise<void> {
-    await message.reply(`Execute command: ${message.content}`);
+    const parsed = parse(message, "!S ", { allowSpaceBeforeCommand: true });
+    if (!parsed.success) return;
+    const number = parseInt(parsed.command)
+    if (!number) return;
+    setTimeout(() => {
+      const channel = this.discordProvider.getClient().channels.cache.get('890194752778174468')
+      if(channel.isText()) {
+        channel.send('Hello!!!')
+      }
+    }, number)
   }
 
-  @On({ event: 'message' })
-  async onMessage(message: Message): Promise<void> {
-    if (!message.author.bot) {
-      const username = message.content.slice(1)
-      const userInfo = await this.userService.findUser( username );
-      if(userInfo) {
-        const {password, __v, ...newUserInfo} = JSON.parse(JSON.stringify(userInfo));
-        const products = newUserInfo.products.map(item => ({title: item.title, price: item.price}))
-        await message.reply(JSON.stringify({...newUserInfo, products}));
-      }else {
-        await message.reply(JSON.stringify(`Sorry. There is no user with name "${username}"`));
-      }
-    }
-  }
+
+  // @On({ event: 'message' })
+  // async onMessage(message: Message): Promise<void> {
+  //   if (!message.author.bot) {
+  //     const username = message.content.slice(1)
+  //     const userInfo = await this.userService.findUser( username );
+  //     if(userInfo) {
+  //       const {password, __v, ...newUserInfo} = JSON.parse(JSON.stringify(userInfo));
+  //       const products = newUserInfo.products.map(item => ({title: item.title, price: item.price}))
+  //       await message.reply(JSON.stringify({...newUserInfo, products}));
+  //     }else {
+  //       await message.reply(JSON.stringify(`Sorry. There is no user with name "${username}"`));
+  //     }
+  //   }
+  // }
 }
